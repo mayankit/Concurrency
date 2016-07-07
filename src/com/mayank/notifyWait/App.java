b140117bf24c652baa5688e0bff3098e7564c774
kumar.mayank <kumar.mayank@snapdeal.com>
2016-07-07 19:08:50 +0530

    Wait and notify example

---
+++ src/com/mayank/notifyWait/App.java
@@ -0,0 +1,52 @@
+package com.mayank.notifyWait;
+
+public class App {
+	
+	private static final Processor processor = new Processor();
+	public static void main(String[] args) {
+
+		
+
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					processor.produce();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					processor.consume();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
---
+++ src/com/mayank/notifyWait/Processor.java
@@ -0,0 +1,30 @@
+package com.mayank.notifyWait;
+
+import java.util.Scanner;
+
+public class Processor {
+
+	public void produce() throws InterruptedException {
+
+		synchronized (this) {
+			System.out.println("Producer thread running.....");
+			wait();
+			System.out.println("Producer resumed");
+		}
+	}
+
+	public void consume() throws InterruptedException {
+		   Scanner scanner  = new Scanner(System.in);
+           Thread.sleep(2000);
+           
+           synchronized (this) {
+        	   System.out.println("Waiting for return key");
+        	   scanner.nextLine();
+        	   System.out.println("Return key pressed...");
+        	   notify();
+        	   Thread.sleep(5000);
+		    }
+          
+	}
+
+}
